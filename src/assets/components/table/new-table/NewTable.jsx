f9fb4cc010a0e5c4dd7df5fa0ab7132cffead202
Andrew-devcoder <andrew.kovpak.01@gmail.com>
2024-01-05 01:25:46 +0200

    add style draft

--- src/assets/components/table/new-table/NewTable.jsx
+++ src/assets/components/table/new-table/NewTable.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { CatFat } from "../../room/cat-fact/CatFact"
 import { TableRows } from "../table-rows/TableRows"
 
+import style from './NewTable.module.scss'
+
 const NewTable = ({ orderNumber }) => {
 	const [addNewRow, setAddNewRow] = useState(false)
 
@@ -19,9 +21,9 @@ const NewTable = ({ orderNumber }) => {
 
 	return (
 		<>
-			<div>
-				hi mom <button onClick={handleAddNewRow}>+</button>
-			</div>
+			<header className={style.header}>
+				<h2>hi mom </h2><button onClick={handleAddNewRow}>+</button>
+			</header>
 
 			<TableRows addNewRow={addNewRow} orderNumber={orderNumber} />
 		</>
