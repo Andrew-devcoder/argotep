7e71b22ea3d941a6ae2a89ed9cffae5c13fced67
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2024-01-05 19:48:56 +0200

    update

--- src/assets/components/room/cat-fact/CatFact.jsx
+++ src/assets/components/room/cat-fact/CatFact.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { getCatFact } from "../../../../services/cats/catfact.ninja"
 import { useEffect } from "react"
 
+import style from './CatFact.module.scss'
+
 const CatFat = ({ addNewRow, currentOrderNumber }) => {
 
 	const [listRows, setListRows] = useState([])
@@ -10,7 +12,19 @@ const CatFat = ({ addNewRow, currentOrderNumber }) => {
 		const fetchData = async () => {
 			try {
 				const fact = await getCatFact();
-				const newRow = <div key={fact}>{currentOrderNumber}. {fact}</div>;
+				const newRow = <div key={fact} className={style.row}>
+					{currentOrderNumber}.
+					<input type="text" placeholder="write name" />
+					<select>
+						<option value="мейнкун">мейнкун</option>
+						<option value="сіамська">сіамська</option>
+						<option value="сфінкс">сфінкс</option>
+						<option value="бірманка">бірманка</option>
+					</select>
+					<input type="number" step='0.1' pattern="^(\d{1,2}(\.\d)?)?$" />
+					<input type="date" />
+					{fact}
+				</div>;
 				setListRows((prevList) => [...prevList, newRow]);
 			} catch (error) {
 				console.error('get fact in component error:', error);
