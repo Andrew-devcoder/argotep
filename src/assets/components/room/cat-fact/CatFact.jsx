35495ba5814b3d10c624e951603247fbdae73be1
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2024-01-06 19:09:14 +0200

    draft

--- src/assets/components/room/cat-fact/CatFact.jsx
+++ src/assets/components/room/cat-fact/CatFact.jsx
@@ -21,10 +21,18 @@ const CatFat = ({ addNewRow, currentOrderNumber }) => {
 						<option value="сфінкс">сфінкс</option>
 						<option value="бірманка">бірманка</option>
 					</select>
-					<input type="number" step='0.1' pattern="^(\d{1,2}(\.\d)?)?$" />
+					<input
+						type="number"
+						step="0.1"
+						min="0.1"
+						max="99.9"
+						value={listRows.length > 0 ? listRows[listRows.length - 1].inputValue : ""}
+						onChange={(e) => handleInputChange(e, listRows.length - 1)}
+					/>
 					<input type="date" />
 					{fact}
 				</div>;
+				setListRows((prevList) => [...prevList, { inputValue: "" }]);
 				setListRows((prevList) => [...prevList, newRow]);
 			} catch (error) {
 				console.error('get fact in component error:', error);
@@ -34,7 +42,25 @@ const CatFat = ({ addNewRow, currentOrderNumber }) => {
 		if (addNewRow) {
 			fetchData();
 		}
-	}, [addNewRow]);
+	}, [addNewRow, currentOrderNumber, inputValue]);
+
+	const handleInputChange = (e, index) => {
+		const value = e.target.value;
+
+		// Перевірка чи не більше однієї цифри після крапки
+		if (value.includes(".") && value.split(".")[1].length > 1) {
+			return; // Ігноруємо введення, якщо умова порушена
+		}
+
+		// Перевірка чи введене значення знаходиться в межах від 0.1 до 99.9
+		if (parseFloat(value) >= 0.1 && parseFloat(value) <= 99.9) {
+			setListRows((prevList) => {
+				const updatedList = [...prevList];
+				updatedList[index] = { ...updatedList[index], inputValue: value };
+				return updatedList;
+			});
+		}
+	};
 
 	return (
 		<>
