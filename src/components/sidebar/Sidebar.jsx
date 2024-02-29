3189fb71c6ca8007a2d877ea9cd2ff8755e06f9f
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2024-02-29 18:07:06 +0200

    fix rerender center area when use sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,17 +4,19 @@ import { useTheme } from '../../state/state'
 
 import style from './Sidebar.module.scss'
 
-const Sidebar = ({ onToggleSidebar }) => {
+const Sidebar = () => {
 	const [toggleSideBar, setToggleSideBar] = useState(false)
 	const { theme } = useTheme()
 
 	const handleButtonClick = () => {
 		setToggleSideBar(!toggleSideBar)
-		onToggleSidebar(!toggleSideBar);
 	}
 
 	return (
-		<div className={style.layout} data-theme={theme}>
+		<div
+			className={style.layout} data-theme={theme}
+			style={{ maxWidth: toggleSideBar ? "10%" : "20%" }}
+		>
 			<div className={style.wrapper}>
 
 				<button className={`${style.toggleButtonOpen} ${toggleSideBar ? style.sidebarOpen : ''}`} onClick={handleButtonClick} />
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,22 +1,12 @@
-import { useState } from 'react';
 import { Sidebar } from '../../components/sidebar/Sidebar';
 import { CentralArea } from '../../components/central-area/CentralArea';
 
 import style from './Home.module.scss'
 
 const Home = () => {
-	const [sidebarOpen, setSidebarOpen] = useState(false);
-
-	const toggleSidebarStyles = (isOpen) => {
-		setSidebarOpen(isOpen);
-		// console.log('click')
-	};
-
-	// edit sidebar 
-
 	return (
-		<div className={`${style.layout} ${sidebarOpen ? style.sidebarOpen : ''}`}>
-			<Sidebar onToggleSidebar={toggleSidebarStyles} />
+		<div className={style.layout}>
+			<Sidebar />
 			<CentralArea />
 		</div>
 	)
